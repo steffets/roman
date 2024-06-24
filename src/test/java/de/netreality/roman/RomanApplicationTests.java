1222f84114e5e006775416527a2fabff5778b20e
Steffets <stefan@sol4web.de>
2024-06-24 13:23:23 +0200

    create

--- src/test/java/de/netreality/roman/RomanApplicationTests.java
+++ src/test/java/de/netreality/roman/RomanApplicationTests.java
@@ -10,4 +10,45 @@ class RomanApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    void generateTest() {
+        String text = "Wir bei der Stuttgarter Versicherung sind die besten Programmierer :)";
+        //String text = "Hallo";
+        for (char c : text.toCharArray()) {
+            int ascii = (int) c;
+            //System.out.print(ascii+ ",");
+           System.out.print(getRomanNumber(ascii) + ",");
+        }
+    }
+
+    @Test
+    void testNumber() {
+        System.out.println(getRomanNumber(1984));
+    }
+
+    private StringBuilder getRomanNumber(int n) {
+        StringBuilder roman = new StringBuilder();
+        int r = divide(roman, n, 1000, 'M', 100, 'C');
+        r = divide(roman, r, 500, 'D', 100, 'C');
+        r = divide(roman, r, 100, 'C', 10, 'X');
+        r = divide(roman, r, 50, 'L', 10, 'X');
+        r = divide(roman, r, 10, 'X', 1, 'V');
+        r = divide(roman, r, 5, 'V', 1, 'I');
+        r = divide(roman, r, 1, 'I', 0, 'I');
+        return roman;
+    }
+
+    private int divide(StringBuilder roman, int number, int divider, char dividerLetter, int sub, char subLetter) {
+        int x = number / divider;
+        for(int i = 0; i < x; i++) {
+            roman.append(dividerLetter);
+            number -= divider;
+        }
+        if (number >= divider - sub) {
+            roman.append(subLetter).append(dividerLetter);
+            number -= divider-sub;
+        }
+        return number;
+    }
+
 }
