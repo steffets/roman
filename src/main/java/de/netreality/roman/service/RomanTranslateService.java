ada33310adb9a32f8f0830d62519a0c6a0208003
Steffets <stefan@sol4web.de>
2024-06-24 13:22:53 +0200

    create

--- src/main/java/de/netreality/roman/service/RomanTranslateService.java
+++ src/main/java/de/netreality/roman/service/RomanTranslateService.java
@@ -2,11 +2,19 @@ package de.netreality.roman.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+
 @Service
 public class RomanTranslateService {
 
     public String translate(String roman) {
         throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String getCode(String arabian) {
+        StringBuilder sb = new StringBuilder();
+        Arrays.stream(arabian.split(",")).forEach(s -> sb.append((char) Integer.valueOf(s).intValue()));
+        return sb.toString();
     }
 
 }
--- src/test/java/de/netreality/roman/service/RomanTranslateServiceTest.java
+++ src/test/java/de/netreality/roman/service/RomanTranslateServiceTest.java
@@ -1,11 +1,13 @@
 package de.netreality.roman.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+@Slf4j
 @ExtendWith(MockitoExtension.class)
 class RomanTranslateServiceTest {
 
@@ -13,10 +15,17 @@ class RomanTranslateServiceTest {
     private RomanTranslateService romanTranslateService;
 
     @Test
-    void translate_I() {
-        String number = romanTranslateService.translate("I");
+    void getCode() {
+        String code = "LXXXVII,CV,CXIV,XXXII,XCVIII,CI,CV,XXXII,C,CI,CXIV,XXXII,LXXXIII,CXVI,CXVII,CXVI,CXVI,CIII,XCVII,CXIV,CXVI,CI,CXIV,XXXII,LXXXVI,CI,CXIV,CXV,CV,XCVX,CIV,CI,CXIV,CXVII,CX,CIII,XXXII,CXV,CV,CX,C,XXXII,C,CV,CI,XXXII,XCVIII,CI,CXV,CXVI,CI,CX,XXXII,LXXX,CXIV,CXI,CIII,CXIV,XCVII,CVX,CVX,CV,CI,CXIV,CI,CXIV,XXXII,LVIII,XLI";
+        String msg = romanTranslateService.getCode( romanTranslateService.translate(code));
+        log.info(msg);
+    }
 
-        Assertions.assertThat(number).isEqualTo("1");
+    @Test
+    void translate_I() {
+        String encoded = romanTranslateService.translate("I");
+
+        Assertions.assertThat(encoded).isEqualTo("1");
     }
 
 }
